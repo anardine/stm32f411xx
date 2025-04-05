6c90645711caaae752474292548d6389a28f9ee4
Alessandro Nardinelli <anardine@hotmail.com>
2025-04-05 20:16:49 -0300

    innitital commit with driver folder configurations

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ /home/anardinelli/STM32CubeIDE/workspace_target/stm32f4xx_driver/drivers/Inc/stm32f411xx.h
+/home/anardinelli/STM32CubeIDE/workspace_target/stm32f4xx_driver/drivers/Inc/stm32f411xx.h:
---
+++ drivers/Inc/stm32f411xx.h
@@ -0,0 +1,21 @@
+/*
+ * stm32f411xx.h
+ *
+ *  Created on: Apr 5, 2025
+ *      Author: anardinelli
+ */
+
+#ifndef INC_STM32F411XX_H_
+#define INC_STM32F411XX_H_
+
+// keep all the base addresses of the microcontroller
+
+#define FASH_BASEADDR				0x0U
+#define SRAM1_BASEADDR				0x0U
+#define SRAM2_BASEADDR				0x0U
+#define ROM_BASEADDR				0x0U
+#define OTP_BASEADDR				0x0U
+#define SRAM 						SRAM1_BASEADD
+
+
+#endif /* INC_STM32F411XX_H_ */
